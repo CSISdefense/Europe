cedef24b988eeb7baa92f67ab1fde9f1b4ee79b9
unknown <mriley@C00146-01-13-15.csis.org>
2015-03-24 11:13:14 -0400

    Finalized the code to create a dataset for regressions. Also uploading the new GDP data and the new neighbor spending data.

--- SSIRegression.R
+++ SSIRegression.R
@@ -1,6 +1,6 @@
 ## Make sure you have installed the packages plm, plyr, and reshape
 
-SSIRegress <- function(filename, regtype, depvar, indvar..., lag = 1) {
+CompilePubOpData <- function(filename, lag = 1) {
     ## start by setting up some items for later use.
     ## Namely, loading needed packages and setting a path to my files.
     require(plm)
@@ -10,53 +10,25 @@ SSIRegress <- function(filename, regtype, depvar, indvar..., lag = 1) {
     ## Set this path to the folder into which your git hub will download data
     path <- "C:/Users/MRiley/My Documents/Europe/"
     
-    ## Next I'm going to load all of my data
-    ## Public Opinion Data
+    ## Next I'm going to load all of my data. The data: in order is..
+    ## public opinion, governance data from PolityIV, Terrorism data from GTD,
+    ## data we compiled on conflicts and a country's membership to the EU,
+    ## GDP per capita data (constant 2005 $), GDP data (also Const 2005 $), population data,
+    ## data on NATO membership, spending data, and neighbor spending data.
     data.a <- read.csv(paste(path, filename, sep =""), header = TRUE) 
     data.gov <- read.csv(paste(path, "SSI_Govern.csv", sep =""), header = TRUE)
     data.ter <- read.csv(paste(path, "Terrorism Data.csv", sep =""), header = TRUE)
     data.conf <- read.csv(paste(path, "Conflict_And_EUmemberstate_Data.csv", sep =""), header = TRUE)
     data.gdppc <- read.csv(paste(path, "SSI_GDPperCAP.csv", sep =""), header = TRUE)
-    data.thrt <- read.csv(paste(path, "SSI_Threat.csv", sep =""), header = TRUE, na.strings = "#VALUE!")
+    data.gdp <- read.csv(paste(path, "SSI_Const05_GDP.csv", sep =""), header = TRUE)
     data.pop <- read.csv(paste(path, "SSI_Population.csv", sep =""), header = TRUE)
     data.nato <- read.csv(paste(path, "SSI_NATO.csv", sep =""), header = TRUE)
     data.euds <- read.csv(paste(path, "EUDefenseSpending_EUROS.csv", sep =""), header = TRUE)
-    
-    ##Samantha's addition of total GDP and nieghborspending variable
-      ##entering in total GDP per country data
-  data.gdp <- read.csv(paste(path, "TotalGDP_EuCountries.csv", sep=""), header = TRUE)
+    data.nghspnd <- read.csv(paste(path, "SSIMilSpendingData.CSV", sep=""), header = TRUE, na.strings = "#VALUE!")
   
-  #changing Column Names 
-  data.gdp1 <- rename(data.gdp, c("X2000"="2000", "X2001"="2001", "X2002"="2002", "X2003"="2003", "X2004"="2004", "X2005"="2005", "X2006"     ="2006", "X2007"="2007", "X2008"="2008", "X2009"="2009", "X2010"="2010", "X2011"="2011", "X2012"="2012", "X2013"="2013"))
-  
-  ##reshaping EU defense spending data 
-  data.gdp2 <- melt(data.gdp1, id=c("Country.Name", "Country.Code", "Indicator.Name", "Indicator.Code"))
-  
-  ##Naming the year column 
-  data.gdp3 <- rename(data.gdp2, c("Country.Name"="Country", "variable"="Year", "value"="GDP2005usd"))
-  
-  ##Sort by country
-  data.gdp <- arrange(data.gdp3, Country)
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  ##entering in Neighbor Spending data
-  
-  data.neighspend <- read.csv(paste(path, "COPY1_SSIMilSpendingData.CSV", sep=""), header = TRUE)
-  
-  ##changing column names
-  data.neighspend1 <- rename(data.neighspend, c("X2000"="2000", "X2001"="2001", "X2002"="2002", "X2003"="2003", "X2004"="2004", "X2005"="2005", "X2006"     ="2006", "X2007"="2007", "X2008"="2008", "X2009"="2009", "X2010"="2010", "X2011"="2011", "X2012"="2012", "X2013"="2013"))
-  
-  ##reshaping data
-  data.neighspend2 <- melt(data.neighspend1, id=c("COUNTRY", "Country.List"))
-  
-  ##naming the year column
-  data.neighspend3 <- rename(data.neighspend2, c("variable"="Year", "value"="neighspend"))
-  
-  ##sort by country
-  data.neighspend4 <- arrange(data.neighspend3, COUNTRY)
 
-    
+#### This next section is where we change the column names of the data sets that don't need
+#### to be reshaped. 
     
     ## Then we change the coloumnames to make them more universal
     colnames(data.gov)[colnames(data.gov)=="country"] <- "Country"
@@ -65,8 +37,6 @@ SSIRegress <- function(filename, regtype, depvar, indvar..., lag = 1) {
     colnames(data.ter)[colnames(data.ter)=="iyear"] <- "Year"
     colnames(data.conf)[colnames(data.conf)=="country"] <- "Country"
     colnames(data.conf)[colnames(data.conf)=="year"] <- "Year"
-    colnames(data.thrt)[colnames(data.thrt)=="COUNTRY"] <- "Country"
-    colnames(data.thrt)[colnames(data.thrt)=="YEAR"] <- "Year"
     data.euds <- rename(data.euds, c("X2001"="2001", "X2002"="2002", "X2003"="2003", "X2004"="2004", "X2005"="2005", "X2006"="2006", "X2007"="2007", "X2008"="2008", "X2009"="2009", "X2010"="2010", "X2011"="2011", "X2012"="2012", "X2013"="2013"))
     colnames(data.gdppc)[colnames(data.gdppc)=="United.Kingdom"] <- "UK"
     colnames(data.gdppc)[colnames(data.gdppc)=="Slovak.Republic"] <- "Slovakia"
@@ -79,44 +49,84 @@ SSIRegress <- function(filename, regtype, depvar, indvar..., lag = 1) {
     colnames(data.euds)[colnames(data.euds)=="Russian.Federation"] <- "Russia"
     colnames(data.euds)[colnames(data.euds)=="Serbia*"] <- "Serbia"
     
+#### In this next component, we will reshape and fit data so we can synthesize with with
+#### the other data that we have.
+    
+  ##reshaping EU defense spending data 
+    data.gdp <- rename(data.gdp, c("X2000"="2000", "X2001"="2001", "X2002"="2002", "X2003"="2003", "X2004"="2004", "X2005"="2005", "X2006"     ="2006", "X2007"="2007", "X2008"="2008", "X2009"="2009", "X2010"="2010", "X2011"="2011", "X2012"="2012", "X2013"="2013"))
+    data.gdp <- melt(data.gdp, id="Country")
+    data.gdp <- rename(data.gdp, c("variable"="Year", "value"="GDP2005usd"))
+    data.gdp <- arrange(data.gdp, Country)
+  
+  
+    ##reshaping neighbor spending data
+    data.nghspnd <- rename(data.nghspnd, c("X2000"="2000", "X2001"="2001", "X2002"="2002", "X2003"="2003", "X2004"="2004", "X2005"="2005", "X2006"     ="2006", "X2007"="2007", "X2008"="2008", "X2009"="2009", "X2010"="2010", "X2011"="2011", "X2012"="2012", "X2013"="2013"))
+    data.nghspnd <- melt(data.nghspnd, id=c("COUNTRY", "Country.List"))
+    data.nghspnd <- rename(data.nghspnd, c("COUNTRY"="Country","variable"="Year", "value"="neighspend"))
+    data.nghspnd <- data.nghspnd[,c(1,3,4)]
+    data.nghspnd <- arrange(data.nghspnd, Country)
+
+    ## Combining Neighbor Spending and GDP data to create a threat ratio variable
+    ## The value of this variable is NghSpnd/GDP
+    threatvariable <- as.data.frame(NULL)
+    c.loop <- unique(data.nghspnd$Country)
+    t.loop <- unique(data.nghspnd$Year)
+  
+    for (i in c.loop) {
+        numerator <- data.nghspnd[data.nghspnd$Country == i,]
+        denominator <- data.gdp[data.gdp$Country == i,]
+            for (t in t.loop){
+                numerator.use <- numerator[numerator$Year == t,]
+                denominator.use <- denominator[denominator$Year == t,]
+                x <- (numerator.use[1,3]/denominator.use[1,3])*1000000
+                row <- data.frame(Country = i, Year = t, ThreatRatio = x)
+                threatvariable <- rbind(threatvariable, row)
+            }
+        
+    }
+    threatvariable$Country <- as.character(threatvariable$Country)
+    threatvariable$Year <- as.integer(as.character(threatvariable$Year))
+
+
     ## We need to reshape and rename the EU defense spending data
     data.euds <- melt(data.euds, id=c("Country", "Unit.Currency"))
-        data.euds <- rename(data.euds, c("variable"="Year", "value"="Total Defense Expenditures"))
-        data.euds <- data.euds[,c(1,3,4)]
-        data.euds[,2] <- as.integer(as.character(data.euds[,2]))
-        data.euds[,3] <- data.euds[,3]*1000000
-        
-    
+    data.euds <- rename(data.euds, c("variable"="Year", "value"="Total Defense Expenditures"))
+    data.euds <- data.euds[,c(1,3,4)]
+    data.euds[,2] <- as.integer(as.character(data.euds[,2]))
+    data.euds$Year <- data.euds$Year-lag
+    data.euds[,3] <- data.euds[,3]*1000000
+    data.euds$Country <- as.character(data.euds$Country)
+    data.euds$Country[data.euds$Country == "United Kingdom"] <- "UK" 
+  
+  
     ## Also need to reshape the GDP per Capita data, and then rename some of the columns
     data.pcap <- melt(data.gdppc, id = "Year")
-        colnames(data.pcap)[colnames(data.pcap)=="year"] <- "Year"
-        colnames(data.pcap)[colnames(data.pcap)=="variable"] <- "Country"
-        colnames(data.pcap)[colnames(data.pcap)=="value"] <- "GDPpCap"
-    
+    colnames(data.pcap)[colnames(data.pcap)=="year"] <- "Year"
+    colnames(data.pcap)[colnames(data.pcap)=="variable"] <- "Country"
+    colnames(data.pcap)[colnames(data.pcap)=="value"] <- "GDPpCap"
+  
     ## Also need to reshape the population data, and then rename some of the columns
     data.population <- melt(data.pop, id = "Year")
-        colnames(data.population)[colnames(data.population)=="year"] <- "Year"
-        colnames(data.population)[colnames(data.population)=="variable"] <- "Country"
-        colnames(data.population)[colnames(data.population)=="value"] <- "Population"
-    
+    colnames(data.population)[colnames(data.population)=="year"] <- "Year"
+    colnames(data.population)[colnames(data.population)=="variable"] <- "Country"
+    colnames(data.population)[colnames(data.population)=="value"] <- "Population"
+  
     ## Also need to reshape the in NATO/alliance, and then rename some of the columns
     data.ally <- melt(data.nato, id = "Year")
-        colnames(data.ally)[colnames(data.ally)=="year"] <- "Year"
-        colnames(data.ally)[colnames(data.ally)=="variable"] <- "Country"
-        colnames(data.ally)[colnames(data.ally)=="value"] <- "NATOally"
-    
+    colnames(data.ally)[colnames(data.ally)=="year"] <- "Year"
+    colnames(data.ally)[colnames(data.ally)=="variable"] <- "Country"
+    colnames(data.ally)[colnames(data.ally)=="value"] <- "NATOally"
+      
     
     ## This next section is to synthesize the terrorism data and boil it down to
     ## the information we actually need for the regression
     data.ter <- data.ter[,2:6]
+    data.ter$Country <- as.character(data.ter$Country)
+    data.ter$Country[data.ter$Country == "Great Britain"] <- "UK"
     countryloop <- sort(unique(data.ter$Country))
     timeloop <- sort(unique(data.ter$Year), decreasing= FALSE)
-    data.terror <- NULL
     attacks <- as.data.frame(NULL)
-    output.a <- NULL
-    sumdom <- NULL
-    sumint <- NULL
-    countrybinder <- NULL
+
     
     complete.ter <- data.ter[complete.cases(data.ter),]
 #     ddply(complete.ter,
@@ -141,14 +151,20 @@ SSIRegress <- function(filename, regtype, depvar, indvar..., lag = 1) {
     out3 <- plyr::join(out2, data.pcap, by = c("Country", "Year"))
     out4 <- plyr::join(out3, data.population, by = c("Country", "Year"))
     out5 <- plyr::join(out4, data.ally, by = c("Country", "Year"))
-    out6 <- plyr::join(out5, data.thrt, by = c("Country", "Year"))
-    out7 <- plyr::join(out6, attacks, by = c("Country", "Year"))
-    out8 <- plyr::join(out7, data.euds, by = c("Country", "Year"))
-    View(out8)  
+    out6 <- plyr::join(out5, attacks, by = c("Country", "Year"))
+    out7 <- plyr::join(out6, data.euds, by = c("Country", "Year"))
+    output <- plyr::join(out7, threatvariable, by = c("Country", "Year"))
+    
+View(output)  
+
+output
+
 
 } 
 
 
-SSIRegress("SSI_DefSpnd_IncDec.csv")
+data.nolag <- CompilePubOpData("SSI_DefSpnd_IncDec.csv", lag = 0)
+data.1lag <- CompilePubOpData("SSI_DefSpnd_IncDec.csv", lag = 1)
+data.2lag <- CompilePubOpData("SSI_DefSpnd_IncDec.csv", lag = 2)
 
 
