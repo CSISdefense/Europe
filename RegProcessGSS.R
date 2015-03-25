7466a36e60e6b14d323dbb005bfbeea4bb7e553a
Greg Sanders <GSanders@csis.org>
2015-03-25 10:52:22 -0400

    RCorr

--- RegProcessGSS.R
+++ RegProcessGSS.R
@@ -1,6 +1,8 @@
+
+
 setwd("K:/Development/Europe") #Your working directory here!
 source("SSIRegression.R")
-
+require("Hmisc")
 
 uslead.1lag <- CompilePubOpData("SSI_US_Leader_Data.csv", lag = 1)
 
@@ -8,6 +10,7 @@ View(use.incdec.1lag)
 
 regdat <- uslead.1lag[34:152,]
 
+regdat$NATOally[is.na(regdat$NATOally)]<-0
 
 Dspend  <- regdat$DefSpnd
 ThrtR <- regdat$ThreatRatio
@@ -21,9 +24,26 @@ Dem <- regdat$democ
 NATO <- regdat$NATOally
 PubOp <- regdat$Spread
 
+reg_df<-data.frame(
+    Dspend  = regdat$DefSpnd,
+    ThrtR = regdat$ThreatRatio,
+    IntAt = regdat$IntAt,
+    DomAt = regdat$DomAt,
+    CivWr = regdat$CivilWar,
+    IntWr=  regdat$IntlCnf,
+    Pop =  regdat$Population,
+    GDPpC = regdat$GDPpCap,
+    Dem = regdat$democ,
+    NATO = regdat$NATOally,
+    PubOp =regdat$Spread
+)
+
+
+
+rcorr(as.matrix(reg_df))
 
 results <- lm(Dspend ~ ThrtR + IntAt + DomAt +CivWr + IntWr + log(Pop) + log(GDPpC) +Dem +NATO +PubOp)
-                  
+
 summary(results)
 
 complete.cases(regdat)
