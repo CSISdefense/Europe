cb3de070fb6686451f84840ca5b6011a93821d12
Greg Sanders <gregory.sanders@gmail.com>
2015-04-05 07:42:29 -0400

    Switching from use of individual vectors to data frames.

--- RegProcessCMR.R
+++ RegProcessCMR.R
@@ -181,7 +181,7 @@ CompilePubOpData <- function(filename, lag = 1) {
 
 ########using US LEADER data
 
-#install.packages("Hmisc")
+# install.packages("Hmisc")
 
 setwd("C:/Users/Greg Sanders/Documents/Development/Europe") #Your working directory here!
 # setwd("C:/Users/scohen/My Documents/Europe1/") #Your working directory here!
@@ -219,12 +219,15 @@ reg_df<-data.frame(
   GDPpC = regdat$GDPpCap,
   Dem = regdat$democ,
   NATO = regdat$NATOally,
-  PubOp =regdat$Spread
+  PubOp =regdat$Spread,
+  Country = regdat$Country,
+  Year = regdat$Year
+  
 )
 
 
 
-rcorr(as.matrix(reg_df))
+rcorr(as.matrix(View(reg_df)))
 
 s1 <- lm(log(Dspend) ~ PubOp, data=reg_df)
 s2 <- lm(log(Dspend) ~ PubOp + ThrtR, data=reg_df)
@@ -255,33 +258,33 @@ require(texreg)
 regdat <- uslead.1lag[34:152,]
 regdat <- comregdat
 
-Dspend <- regdat$DefSpnd
-ThrtR <- regdat$ThreatRatio
-IntAt <- regdat$IntAt
-DomAt <- regdat$DomAt
-CivWr <- regdat$CivilWar
-IntWr <- regdat$IntlCnf
-Pop <- regdat$Population
-GDPpC <- regdat$GDPpCap
-Dem <- regdat$democ
-NATO <- regdat$NATOally
-PubOp <- regdat$Spread
+# Dspend <- regdat$DefSpnd
+# ThrtR <- regdat$ThreatRatio
+# IntAt <- regdat$IntAt
+# DomAt <- regdat$DomAt
+# CivWr <- regdat$CivilWar
+# IntWr <- regdat$IntlCnf
+# Pop <- regdat$Population
+# GDPpC <- regdat$GDPpCap
+# Dem <- regdat$democ
+# NATO <- regdat$NATOally
+# PubOp <- regdat$Spread
 
 
-summary(Dspend)
-summary(log(Dspend))
-summary(ThrtR)
-summary(IntAt)
-summary(DomAt)
-summary(CivWr)
-summary(IntWr)
-summary(Pop)
-summary(GDPpC)
-summary(Dem)
-summary(NATO)
-summary(PubOp)
-summary(log(Pop))
-summary(log(GDPpC))
+summary(reg_df$Dspend)
+summary(log(reg_df$Dspend))
+summary(reg_df$ThrtR)
+summary(reg_df$IntAt)
+summary(reg_df$DomAt)
+summary(reg_df$CivWr)
+summary(reg_df$IntWr)
+summary(reg_df$Pop)
+summary(reg_df$GDPpC)
+summary(reg_df$Dem)
+summary(reg_df$NATO)
+summary(reg_df$PubOp)
+summary(log(reg_df$Pop))
+summary(log(reg_df$GDPpC))
 
 
 
@@ -291,26 +294,28 @@ summary(log(GDPpC))
 library(MASS)
 
 
-Aresults1 <- lm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data = regdat)
+Aresults1 <- lm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data = reg_df)
 screenreg(list(Aresults1))
 
-USstep <- stepAIC(Aresults1, direction = "backward")
+
+CAresults1 <- lm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data = reg_df[complete.cases(reg_df),])
+USstep <- stepAIC(CAresults1, direction = "backward")
 USstep$anova
 ###State Fixed Effects Model
 
-Aresults2 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data=regdat, index=c("Country", "Year"), model="within")
+Aresults2 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data=reg_df, index=c("Country", "Year"), model="within")
 summary(Aresults2)
 screenreg(list(Aresults1, Aresults2))
 
 ##Time Fixed Effects Model
 
-Aresults3 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data=regdat, index=c("Country", "Year"), effect="time")
+Aresults3 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data=reg_df, index=c("Country", "Year"), effect="time")
 summary(Aresults3)  
 screenreg(list(Aresults1, Aresults2, Aresults3))
 
 ##PROBLEMS fixed and time fixed effects model returns ERROR: "Error in crossprod(t(X), beta) : non-conformable arguments"
 ##State fixed and time fixed effects Model
-Aresults4 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + GDPpC + Dem + NATO + PubOp, data=regdat, index=c("Country", "Year"), effect="twoways", model="within")
+Aresults4 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + GDPpC + Dem + NATO + PubOp, data=reg_df[complete.cases(reg_df),], index=c("Country", "Year"), effect="twoways", model="within")
 summary(Aresults4)
 screenreg(list(Aresults1, Aresults2, Aresults3))
 
@@ -368,13 +373,13 @@ screenreg(list(Bresults1, Bresults2))
 
 ##Time Fixed Effects Model
 
-Bresults3 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data=regdat1, index=c("Country", "Year"), effect="time")
+Bresults3 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + log(GDPpC) + Dem + NATO + PubOp, data=reg_df, index=c("Country", "Year"), effect="time")
 summary(Bresults3)  
 screenreg(list(Bresults1, Bresults2, Bresults3))
 
 ##PROBLEMS fixed and time fixed effects model returns ERROR: "Error in crossprod(t(X), beta) : non-conformable arguments"
 ##State fixed and time fixed effects Model
-Bresults4 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + GDPpC + Dem + NATO + PubOp, data=regdat1, index=c("Country", "Year"), effect="twoways", model="within")
+Bresults4 <- plm(log(Dspend) ~ ThrtR + IntAt + DomAt + CivWr + IntWr + Pop + GDPpC + Dem + NATO + PubOp, data=reg_df[complete.cases(reg_df),], index=c("Country", "Year"), effect="twoways", model="within")
 summary(Bresults4)
 screenreg(list(Bresults1, Bresults2, Bresults3))
 
